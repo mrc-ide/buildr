64dd4fabc22d5087978e87346d8d5fc6731ac708
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-01-22 20:06:50 +0000

    Client support

---
+++ R/buildr_client.R
@@ -0,0 +1,109 @@
+buildr_client <- function(host="localhost", port=8765) {
+  .R6_buildr_http_client$new(host, port)
+}
+
+.R6_buildr_http_client <- R6::R6Class(
+  "buildr_http_client",
+  public=list(
+    base_url=NULL,
+    initialize=function(host, port) {
+      self$base_url <- sprintf("http://%s:%d", host, port)
+    },
+
+    packages=function(binary=FALSE) {
+      type <- if (binary) "binary" else "source"
+      r <- httr::GET(file.path(self$base_url, "packages", type))
+      buildr_http_client_response(r, empty=character(0))
+    },
+
+    status=function(hash) {
+      r <- httr::GET(file.path(self$base_url, "status", hash))
+      buildr_http_client_response(r)
+    },
+
+    binary=function(hash, dest=tempfile()) {
+      dir.create(dest, FALSE, TRUE)
+      if (!file.info(dest, extra_cols=FALSE)[["isdir"]]) {
+        stop("dest must be a directory")
+      }
+      r <- httr::GET(file.path(self$base_url, "binary", hash))
+      dat <- buildr_http_client_response(r)
+      ret <- file.path(dest, self$binary_filename(hash))
+      writeBin(dat, ret)
+      ret
+    },
+
+    submit=function(filename) {
+      if (!file.exists(filename)) {
+        stop("Cannot find file at ", filename)
+      }
+      r <- httr::POST(file.path(self$base_url, "submit", basename(filename)),
+                      body=httr::upload_file(filename))
+      buildr_http_client_response(r)
+    },
+
+    ## This is not quite the right response because we really want to
+    ## do the right thing on error too.  Because R will handle the
+    ## requests one after another we can't really do much about this
+    ## but wait.
+    ##
+    ## Try breaking the package and seeing what is returned here.
+    wait=function(hash, dest=tempfile(), poll=1, timeout=60) {
+      t_end <- Sys.time() + timeout
+      repeat {
+        ok <- tryCatch(cl$binary_filename(hash),
+                       error=function(e) NULL)
+        if (is.null(ok)) {
+          if (Sys.time() > t_end) {
+            cat(self$log(hash))
+            stop("Package not created in time")
+          }
+          Sys.sleep(poll)
+        } else if (ok == "") {
+          stop(sprintf("Build failed; see '$log(\"%s\")' for details", hash))
+        } else {
+          return(self$binary(hash, dest))
+        }
+      }
+    },
+
+    log=function(hash) {
+      r <- httr::GET(file.path(self$base_url, "log", hash))
+      log <- buildr_http_client_response(r)
+      class(log) <- "build_log"
+      log
+    },
+
+    binary_filename=function(hash) {
+      r <- httr::GET(file.path(self$base_url, "binary_filename", hash))
+      buildr_http_client_response(r)
+    },
+
+    queue_status=function() {
+      r <- httr::GET(file.path(self$base_url, "queue_status"))
+      buildr_http_client_response(r)
+    }))
+
+##' @export
+print.build_log <- function(x, ...) {
+  writeLines(x)
+}
+
+buildr_http_client_response <- function(r, empty=list()) {
+  httr::stop_for_status(r)
+  type <- httr::headers(r)$"content-type"
+  if (type == "application/json") {
+    x <- httr::content(r, "text", encoding="UTF-8")
+    from_json(x, empty)
+  } else if (type == "application/octet-stream") {
+    httr::content(r, "raw")
+  } else if (type == "text/plain") {
+    httr::content(r, "text", encoding="UTF-8")
+  } else {
+    stop("Unexpected response type")
+  }
+}
+
+from_json <- function(x, empty=list()) {
+  if (x == "[]") empty else jsonlite::fromJSON(x)
+}
