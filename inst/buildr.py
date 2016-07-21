22d40f3dfb61d33a97b3bbf294d3c07974b15cc9
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-07-21 16:49:43 +0100

    Prototype python buildr/server pair

---
+++ inst/buildr.py
@@ -0,0 +1,177 @@
+import hashlib
+import json
+import os
+import shutil
+import subprocess
+import time
+
+# All the R stuff will be done async.  So we'll keep a queue of things
+# to run and keep track of when it has completed, while avoiding
+# blocking.  This won't be amazingly lovely, but it should work, and
+# we're only after something simple here.
+
+# Directories that we need:
+#
+#   lib - the R library that we'll accumulate
+#   source - a directory of source packages
+#   binary - a directory of binary packages
+#   log - a directory of log files, from each build
+#   info - a place to stash json information
+#   filenames - where original filenames are stored
+
+def paths(root):
+    return {'root':     root,
+            'lib':      os.path.join(root, 'lib'),
+            'source':   os.path.join(root, 'source'),
+            'binary':   os.path.join(root, 'binary'),
+            'info':     os.path.join(root, 'info'),
+            'filename': os.path.join(root, 'filename'),
+            'incoming': os.path.join(root, 'incoming'),
+            'log':      os.path.join(root, 'log')}
+
+
+def md5sum(filename):
+    hasher = hashlib.md5()
+    with open(filename, 'rb') as file:
+        hasher.update(file.read())
+    return hasher.hexdigest()
+
+
+def dir_create(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def process(id, args, log):
+    fh = None if log is None else open(log, 'w')
+    process = subprocess.Popen(args, stdout = fh, stderr = fh)
+    return {'id': id, 'process': process, 'log': log, 'log_handle': fh}
+
+
+def package_types():
+    return ('source', 'binary')
+
+
+class Buildr:
+    def __init__(self, path='.'):
+        # should spawn R here to check packages I think, and set up a
+        # temporary library, as we don't want to fuck up the main one.
+        self.paths = paths(path)
+        for p in self.paths.itervalues():
+            dir_create(p)
+        code = subprocess.call(
+            ['Rscript', '-e', 'buildr:::bootstrap("%s")' % self.paths['lib']])
+        if code != 0:
+            raise Exception('Error running bootstrap script')
+        os.environ['R_LIBS_USER'] = self.paths['lib']
+        self.queue = []
+        self.active = None
+        self.log('buildr', 'starting')
+
+    def package_status(self, package_id):
+        if os.path.exists(os.path.join(self.paths['binary'], package_id)):
+            return 'COMPLETE'
+        elif os.path.exists(os.path.join(self.paths['info'], package_id)):
+            return 'ERROR'
+        elif package_id in self.queue:
+            return 'PENDING'
+        elif os.path.exists(os.path.join(self.paths['source'], package_id)):
+            return 'RUNNING'
+        else:
+            return 'UNKNOWN'
+
+    def package_info(self, package_id):
+        filename_info = os.path.join(self.paths['info'], package_id)
+        if not os.path.exists(filename_info):
+            return None
+        return json.loads(read_file(filename_info))
+
+    def source_info(self, package_id):
+        filename_source = os.path.join(self.paths['filename'], package_id)
+        if not os.path.exists(filename_source):
+            return None
+        return {'hash_source': package_id,
+                'filename_source': read_file(filename_source)}
+
+    def queue_status(self):
+        return self.queue
+
+    def queue_build(self, filename):
+        package_id = md5sum(filename)
+        with open(os.path.join(self.paths['filename'], package_id), 'w') as f:
+            f.write(os.path.basename(filename))
+        filename_source = os.path.join(self.paths['source'], package_id)
+        filename_binary = os.path.join(self.paths['binary'], package_id)
+        if os.path.exists(filename_binary) or package_id in self.queue:
+            self.log(package_id, 'skipping')
+        else:
+            shutil.copyfile(filename, filename_source)
+            self.log(package_id, 'queuing')
+            self.queue.append(package_id)
+        return package_id
+
+    def queue_upgrade(self):
+        run = False
+        if 'upgrade' in self.queue:
+            self.log('upgrade', 'skipping - already queued')
+        elif self.active and self.active['id'] is not 'upgrade':
+            self.log('upgrade', 'skipping - already running')
+        else:
+            run = True
+            self.log('upgrade', 'queuing')
+            self.queue.insert(0, 'upgrade')
+        return run
+
+    def run_upgrade(self):
+        args = ['Rscript', '-e',
+                'update.packages("%s", ask=FALSE)' % self.paths['lib']]
+        return process('upgrade', args, None)
+
+    def run_build(self, package_id):
+        args = ['Rscript', '-e',
+                'buildr:::build_binary_main("%s", "%s", "%s", "%s")' % (
+                    package_id, self.paths['source'], self.paths['binary'],
+                    self.paths['info'])]
+        logfile = os.path.join(self.paths['log'], package_id)
+        return process(package_id, args, logfile)
+
+    def run(self):
+        if self.active:
+            p = self.active['process'].poll()
+            if p is None:
+                self.log(self.active['id'], 'still running')
+                return None
+            self.cleanup(p)
+        self.run_next()
+
+    def run_next(self):
+        if len(self.queue) == 0:
+            return None
+        else:
+            id = self.queue.pop()
+            self.log(id, 'starting')
+            if id == 'upgrade':
+                self.active = self.run_upgrade()
+            else:
+                self.active = self.run_build(id)
+
+    def cleanup(self, p):
+        self.log(self.active['id'], 'complete with code %d' % p)
+        if self.active['log_handle']:
+            self.active['log_handle'].close()
+        id = self.active['id']
+        if id != 'upgrade' and p != 0:
+            with open(os.path.join(self.paths['info'], id), 'w') as file:
+                file.write(json.dumps({'id': id, 'hash': id}))
+        self.active = None
+
+    def log(self, id, message):
+        log_str = '[%s] (%-32s) %s' % (
+            time.strftime('%Y-%m-%d %H:%M:%S'), id, message)
+        with open(os.path.join(self.paths['log'], 'queue'), 'a') as logfile:
+            logfile.write(log_str + "\n")
+        print log_str
+
+def read_file(filename):
+    with open(filename, 'r') as f:
+        return f.read()
---
+++ inst/server.py
@@ -0,0 +1,108 @@
+# To debug;
+#   import ipdb; ipdb.set_trace()
+import os
+import flask
+from werkzeug import secure_filename
+
+import buildr
+
+app = flask.Flask(__name__)
+obj = buildr.Buildr()
+
+@app.route('/')
+def index():
+    return flask.jsonify('This is buildr')
+
+@app.route("/packages/<package_type>")
+def packages(package_type):
+    check_package_type(package_type)
+    packages = os.listdir(obj.paths[package_type])
+    return flask.jsonify(packages)
+
+@app.route("/status/<package_id>")
+def status(package_id):
+    if package_id == "queue":
+        ret = obj.queue_status()
+    else:
+        ret = obj.package_status(package_id)
+    return flask.jsonify(ret)
+
+@app.route("/info/<package_id>")
+def info(package_id):
+    ret = obj.package_info(package_id)
+    if ret:
+        return flask.jsonify(ret)
+    else:
+        return err_not_found()
+
+@app.route("/source_info/<package_id>")
+def source_info(package_id):
+    ret = obj.source_info(package_id)
+    if ret:
+        return flask.jsonify(ret)
+    else:
+        return err_not_found()
+
+@app.route("/log/<package_id>")
+def log(package_id):
+    filename = obj.paths['log'] + '/' + package_id
+    if not os.path.exists(filename):
+        return err_not_found()
+    with open(filename, 'r') as f:
+        return flask.jsonify(f.read())
+
+@app.route("/download/<package_id>/<package_type>")
+def download(package_id, package_type):
+    check_package_type(package_type)
+    path = os.path.join(obj.paths[package_type], package_id)
+    if not os.path.exists(path):
+        return err_not_found()
+    return flask.send_file(os.path.abspath(path),
+                           'application/octet-stream')
+
+@app.route("/submit/<package_name>", methods=["POST"])
+def submit(package_name):
+    tmpfile = os.path.join(obj.paths['root'], 'incoming',
+                           secure_filename(os.path.basename(package_name)))
+    with open(tmpfile, 'wb') as file:
+        file.write(flask.request.data)
+    package_id = obj.queue_build(tmpfile)
+    os.remove(tmpfile)
+    return flask.jsonify(package_id)
+
+@app.route("/upgrade", methods=["PATCH"])
+def upgrade():
+    return flask.jsonify(obj.queue_upgrade())
+
+@app.after_request
+def process_queue(response):
+    obj.run()
+    return response
+
+def err_not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not found: ' + flask.request.url
+    }
+    resp = flask.jsonify(message)
+    resp.status_code = 404
+    return resp
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+def check_package_type(package_type):
+    if package_type not in buildr.package_types():
+        raise InvalidUsage("Invalid package type", status_code=400)
